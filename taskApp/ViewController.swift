1e4c7ae33326f57ecb64395de32ebea7fe3ed070
Anna Takegawa <anna@AnnanoMacBook-Pro.local>
2018-06-21 18:11:43 +0900

    Initial Commit

---
+++ taskApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  taskApp
+//
+//  Created by Anna Takegawa on 6/21/18.
+//  Copyright © 2018 anna.takegawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
